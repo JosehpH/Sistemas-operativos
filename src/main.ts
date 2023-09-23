b76dade19e781d6a3be06f7e37768166efb669fb
JosehpH <u20211e098@upc.edu.pe>
2023-09-22 21:49:48 -0500

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app: INestApplication | any = await NestFactory.create(AppModule);
+  const app: INestApplication | any = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('LiveBi')
